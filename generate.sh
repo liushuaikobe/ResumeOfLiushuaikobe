d272beaed9d60433395eb9ffa12a81c5e512c0c3
liushuaikobe <liushuaikobe@gmail.com>
2014-07-07 23:08:22 +0800

    update at 2014-07-07 23:08:22,3N

--- generate.sh
+++ generate.sh
@@ -1,13 +1,17 @@
 node ./node_modules/resume-cli/index.js export resume.pdf
 node ./node_modules/resume-cli/index.js export resume.html
+git add -A .
+git commit -m "update at `date +"%Y-%m-%d %H:%M:%S,%3N"`"
+git push origin master
 
 cp resume.html /tmp/resume.html
 git checkout gh-pages
-mv /tmp/resume.html .
+mv /tmp/resume.html ./index.html
+git add -A .
+git commit -m "update at `date +"%Y-%m-%d %H:%M:%S,%3N"`"
 git push origin gh-pages
 
 git checkout master 
-git push origin master
 
 
 
